15937d2187d1b970f90cc29d186ac7eb9fb604fb
Brandon Mailhiot <brandon.mailhiot@gmail.com>
2017-07-30 10:49:50 -0700

    Update About.jsx

--- js/components/About.jsx
+++ js/components/About.jsx
@@ -10,12 +10,7 @@ export default class About extends React.Component{
     super(props)
 
     this.state = {
-      links: [
-        ['behance', 'Behance', '.net/blmgeo'],
-        ['github', 'GitHub', '.com/blmgeo'],
-        ['linkedin', 'LinkedIn', '.com/in/brandonmailhiot'],
-        ['twitter', 'Twitter', '.com/blmgeo']
-      ]
+      links: []
     }
   }
   render(){
@@ -26,22 +21,14 @@ export default class About extends React.Component{
           <div className='about-info'>
             <h1 className='about-title'>About</h1>
             <p className='about-description about-intro'>
-              Brandon Mailhiot is a front end developer passionate about creating exceptional user experiences.
-              He has contributed to the web for over three years, developing skills in user interface design and development.
-              To further this end, he writes about recent trends and advancements in technology. In his spare time,
-              he enjoys reading and songwriting.
             </p>
             <p className='about-description about-intro'>
-              Want to get in touch? Visit his <Link className='about-link' to="/contact">Contact</Link> page or check out his
-              social profiles below.
             </p>
           </div>
         </main>
         <AboutSquare title='This Site'>
           <div className='about-info'>
           <p className='about-description about-intro'>
-            The frontend of this website is built with a React.js + Redux architecture, which interfaces with a Node.js + Express +
-            React-Router backend. Blog content is served via the <Link className='about-link' to='/projects'>RailsCMS</Link> service.
           </p>
           </div>
         </AboutSquare>
